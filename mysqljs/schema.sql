9e50e8fef43f705eaf34ba4762f0daec966a638b
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-09-27 23:59:42 -0400

    Reverting to previous commit.

--- mysqljs/schema.sql
+++ mysqljs/schema.sql
@@ -4,4 +4,12 @@ USE passport_demo;
 
 SELECT * FROM passport_demo.users;
 
+CREATE TABLE datainput (
+  id INT NOT NULL AUTO_INCREMENT,
+  expensed VARCHAR(45) NULL,
+  expenset VARCHAR(100) NULL,
+  expensea DECIMAL (10,2) NULL,
+  PRIMARY KEY (id)
+);
 
+INSERT INTO datainput (expensed, expenset, expensea)
