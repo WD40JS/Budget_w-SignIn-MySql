589c9016fc6674d92c05275c8223bb2f467578ba
nnismithllc <65423204+nnismithllc@users.noreply.github.com>
2020-09-28 17:42:50 -0400

    updated all working files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,7 +28,7 @@ module.exports = function(app) {
       })
   });
 
-  app.get("/members", (req, res) => {
+  app.get("/members", isAuthenticated, (req, res) => {
     // If the user already has an account send them to the members page
     if (!req.user) {
       res.redirect("/members");
@@ -41,7 +41,7 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/members.handlebars"));
-  });
+  // app.get("/members", isAuthenticated, (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./views/members.handlebars"));
+  // });
 };
